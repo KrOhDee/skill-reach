0304d6b57dcbf552cb42e8394477743c3c9d2c2d
Luis Aleman <ljdev24@gmail.com>
2025-02-20 20:21:35 -0500

    Setup css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: colors.gray[800],
+        secondary: colors.white,
       },
     },
   },
