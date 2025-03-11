18ce5c1b4dbf977273e8ad6bf12c135c4245a628
Luis Aleman <ljdev24@gmail.com>
2025-03-11 19:58:09 -0400

    Add styling to ImageTextRow component

--- app/_components/ImageTextRow.tsx
+++ app/_components/ImageTextRow.tsx
@@ -14,9 +14,15 @@ export default function ImageTextRow({
   children,
 }: ImageTextRowProps) {
   return (
-    <div className={`flex ${isReversed ? 'flex-row-reverse' : 'flex-row'}`}>
-      <Image src={imageSrc} alt={alt} />
-      <div>{children}</div>
+    <div
+      className={`lg:flex flex-1 items-center gap-5 ${
+        isReversed ? 'flex-row-reverse' : 'flex-row'
+      }`}
+    >
+      <div className='lg:w-1/3 flex justify-center'>
+        <Image src={imageSrc} alt={alt} width={250} height={250} />
+      </div>
+      <div className='lg:w-2/3 text-xl'>{children}</div>
     </div>
   );
 }
